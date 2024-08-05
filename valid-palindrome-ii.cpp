144eb2f17f4c54a36597a34f2909deeea6509d80
TejasVijaya74 <vt6267700@gmail.com>
2024-08-05 19:15:48 +0530

    intialzed code

---
+++ valid-palindrome-ii.cpp
@@ -0,0 +1,28 @@
+/*Given a string s, return true if the s can be palindrome after deleting at most one character from it.*/
+
+class Solution {
+public:
+    bool isPalindrome(const string& s, int left, int right) {
+        while (left < right) {
+            if (s[left] != s[right]) {
+                return false;
+            }
+            left++;
+            right--;
+        }
+        return true;
+    }
+
+    bool validPalindrome(string s) {
+        int left = 0, right = s.length() - 1;
+        while (left < right) {
+            if (s[left] != s[right]) {
+                // Check by removing either the left character or the right character
+                return isPalindrome(s, left + 1, right) || isPalindrome(s, left, right - 1);
+            }
+            left++;
+            right--;
+        }
+        return true;
+    }
+};
